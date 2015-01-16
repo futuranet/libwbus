5612a6563842bb12d464bc3599aefaae7dc88d9b
nobody <mjander@users.sourceforge.net>
2015-01-16 01:21:59 +0100

    Add reception level and error rate to GSM status.

--- openegg/gsmctl.c
+++ openegg/gsmctl.c
@@ -7,6 +7,7 @@
 #include "machine.h"
 #include "kernel.h"
 #include <string.h>
+#include <stdio.h>
 
 #ifdef __MSP430_449__
 #define GSM_BAUDRATE 19200
@@ -34,6 +35,9 @@ struct gsmctl{
   char (*allowedNumbers)[32];
   unsigned char flags;
   unsigned char gotIdx;
+  /* Signal quality indices */
+  unsigned char rssi;
+  unsigned char ber;
 };
 
 static char tmp[32+1];
@@ -43,24 +47,46 @@ static char tmp[32+1];
  */
 static int gsmctl_atio(HANDLE_GSMCTL hgsmctl, char *cmd, char *result)
 {
-  int bytes;
+  int bytes, tmp;
 
   bytes = strlen(cmd);
   rs232_flush(hgsmctl->rs232);  
   rs232_write(hgsmctl->rs232, (unsigned char*)cmd, bytes);
-  /* Give AT command enough time */
-  kernel_sleep(MSEC2JIFFIES(100));
-  kernel_yield();      
   /* Skip command echo */
-  rs232_read(hgsmctl->rs232, (unsigned char*)result, bytes);
+  kernel_sleep(MSEC2JIFFIES(500));
+  kernel_yield();
+  tmp = rs232_read(hgsmctl->rs232, (unsigned char*)result, bytes);
+  /* Give AT command enough time. */
+  for (tmp=0; tmp<50; tmp++) {
+    kernel_sleep(MSEC2JIFFIES(100));
+    kernel_yield();
+    if (rs232_rxBytes(hgsmctl->rs232) > 2) {
+      break;
+    }
+  }
   /* Read result data */
-  bytes = rs232_read(hgsmctl->rs232, (unsigned char*)result, 32);
-
+  for (bytes=0; bytes<32; bytes++)
+  {
+    tmp = rs232_read(hgsmctl->rs232, (unsigned char*)result+bytes, 1);
+    /* break if no data */
+    if (tmp != 1) {
+      bytes++;
+      break;
+    }
+    if (bytes > 0) {
+      /* Break if OK returned */
+      if (result[bytes-1] == 'O' && result[bytes] == 'K') {
+        tmp = 2;
+        bytes++;
+        break;
+      }
+    }
+  }
   /* Null terminate result */
   result[bytes] = 0;
 
-  if (strstr(result, "OK") == NULL || bytes == 0) {
-    return -1;
+  if (tmp != 2) {
+    bytes = -1;
   }
 
   return bytes;
@@ -126,18 +152,20 @@ int gsmctl_init(HANDLE_GSMCTL hgsmctl)
     /* Check status to be either 1 (home network) or 5 (roaming) */
     /* \r\n+CREG: 0,1\r\n\r\nOK\r\n */
     ptr = strstr(tmp, "0,");
-    if (ptr != NULL) {
-      if (ptr[2] == '1') { 
-        hgsmctl->flags &= ~GSMCTL_DENIED;
+    if (ptr != NULL)
+    {
+      switch (ptr[2]) {
+      case '5':
+        hgsmctl->flags |= GSMCTL_ROAMING;
+      case '1': 
         hgsmctl->flags |= GSMCTL_REGISTERED;
-      }
-      if (ptr[2] == '5') {
         hgsmctl->flags &= ~GSMCTL_DENIED;
-        hgsmctl->flags |= GSMCTL_REGISTERED|GSMCTL_ROAMING;
-      }
-      if (ptr[2] == '3') {
+        break;
+      case '3':
         hgsmctl->flags |= GSMCTL_DENIED;
+      default:
         hgsmctl->flags &= ~(GSMCTL_REGISTERED|GSMCTL_ROAMING);
+        break;
       }
     } else {
       hgsmctl->flags &= ~(GSMCTL_REGISTERED|GSMCTL_ROAMING|GSMCTL_DENIED);
@@ -146,6 +174,34 @@ int gsmctl_init(HANDLE_GSMCTL hgsmctl)
 
   if ((hgsmctl->flags & GSMCTL_REGISTERED) == 0) {
     return -2;
+  }
+
+  /* Get some info about link quality. This should tell you if the antenna is OK. */
+  if (hgsmctl->flags & GSMCTL_REGISTERED)
+  {
+    gsmctl_atio(hgsmctl, "at+csq\r", tmp);
+    {
+      char *ptr = tmp;
+      
+      /* \r\n+CSQ: 22,0\r\n\r\nOK\0 */
+      while (ptr < (tmp+32) && *ptr != ',') {
+        ptr++;
+      }
+      if (*ptr == ',')
+      {
+        bytes = ptr[-1] - '0';
+        if (ptr[-2] > '0' && ptr[-2] <= '9') {
+          bytes += 10*(ptr[-2]-'0');
+        }
+        hgsmctl->rssi = bytes;
+
+        bytes = ptr[1] - '0';
+        if (ptr[2] >= '0' && ptr[2] <= '9') {
+          bytes = bytes*10 + (ptr[2] - '0');
+        }
+        hgsmctl->ber = bytes;
+      }
+    }
   }
 
   return 0;
@@ -189,23 +245,34 @@ int gsmctl_getTime(HANDLE_GSMCTL hgsmctl, rtc_time_t *time)
 
 void gsmctl_getStatus(HANDLE_GSMCTL hgsmctl, char *stext)
 {
+  /* Note: this function does not do any serial I/O so that
+  this function can be called from another thread too. */
   switch (hgsmctl->flags & (GSMCTL_POWERON|GSMCTL_REGISTERED|GSMCTL_ROAMING|GSMCTL_DENIED)) {
     case 0:
       strcpy(stext, "Off");
       break;
     case GSMCTL_POWERON:
-      strcpy(stext, "searching");
+      strcpy(stext, "Searching");
       break;
     case GSMCTL_POWERON|GSMCTL_REGISTERED:
-      strcpy(stext, "registered");
+      strcpy(stext, "Regd");
       break;
     case GSMCTL_POWERON|GSMCTL_REGISTERED|GSMCTL_ROAMING:
-      strcpy(stext, "roaming");
+      strcpy(stext, "Roam");
       break;
     case GSMCTL_DENIED:
-      strcpy(stext, "denied");
+      strcpy(stext, "Denied");
       break;
   }
+
+  if (hgsmctl->flags & GSMCTL_REGISTERED)
+  {
+    /* S  -65dB E 00 % */
+    sprintf(stext, "S %4ddB E %02d %%", -113 + hgsmctl->rssi*2, hgsmctl->ber*2);
+    /* apply 1/10 by shifting digits. */
+    stext[13] = stext[12];
+    stext[12] = '.';
+  }  
 }
 
 /*
@@ -216,7 +283,7 @@ void gsmctl_addNumber(HANDLE_GSMCTL hgsmctl)
   hgsmctl->no_io_timeout = 0; /* Stop lock out */
   hgsmctl->flags &= ~GSMCTL_GOTNUMBER;
   hgsmctl->flags |= GSMCTL_ADDNUMBER;
-  hgsmctl->reset_timeout = 60; /* Give up after 1 minutes */
+  hgsmctl->reset_timeout = 120; /* Give up after 2 minutes */
 }
 
 void gsmctl_addNumberCancel(HANDLE_GSMCTL hgsmctl)
